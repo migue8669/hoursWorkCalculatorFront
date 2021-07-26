57fee70dd0674dbe348961e9a9ae90562cd8d340
migue8669 <miguelramirez8669@gmail.com>
2021-07-25 20:40:16 -0500

    refactor en inputs en report

--- src/app/pages/report/report.component.ts
+++ src/app/pages/report/report.component.ts
@@ -1,8 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormGroupDirective } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  FormGroupDirective,
+  Validators,
+} from '@angular/forms';
 import { ComponentsService } from 'src/app/services/components.service';
 import { ReportService } from 'src/app/services/report.service';
 import { UUID } from 'angular2-uuid';
+import { not } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-report',
@@ -10,17 +17,18 @@ import { UUID } from 'angular2-uuid';
   styleUrls: ['./report.component.css'],
 })
 export class ReportComponent implements OnInit {
-  uuidValue!:string;
+  uuidValue!: string;
 
   reportObject: any;
   contador: number = 0;
-  msgCalculator: number=0;
+  msgCalculator: number = 0;
 
-
-
-  constructor(private reportService: ReportService, private componetService: ComponentsService) {}
+  constructor(
+    private reportService: ReportService,
+    private componetService: ComponentsService
+  ) {}
   reportForm = new FormGroup({
-    technicianIdentity: new FormControl(''),
+    technicianIdentity: new FormControl('', Validators.required),
     reportIdentityNumber: new FormControl(''),
     dateInit: new FormControl(''),
     dateFinish: new FormControl(''),
@@ -30,81 +38,90 @@ export class ReportComponent implements OnInit {
   });
   ngOnInit(): void {
     this.getReport();
-this.componetService.customMessage.subscribe(msg=>{this.msgCalculator=msg;
-  console.log(this.msgCalculator)}
-  ) 
-if(this.msgCalculator){
-  this.reportService.get(this.msgCalculator).subscribe(e=>{console.log(e.items)
-for (const iterator of e.items) {
-  console.log(iterator.technicianIdentity)
+    this.componetService.customMessage.subscribe((msg) => {
+      this.msgCalculator = msg;
+      console.log(this.msgCalculator);
+    });
+    if (this.msgCalculator) {
+      this.reportService.get(this.msgCalculator).subscribe((e) => {
+        console.log(e.items);
+        for (const iterator of e.items) {
+          console.log(iterator.technicianIdentity);
 
-      this.reportForm = this.reportObject[iterator.technicianIdentity];
-      this.reportForm = new FormGroup({
-     //   reportIdentityNumber: new FormControl(e.reportIdentityNumber),
-     technicianIdentity: new FormControl(iterator.technicianIdentity.value),
-        dateInit: new FormControl(iterator.dateInit),
-        dateFinish: new FormControl(iterator.dateFinish),
-        hourInit: new FormControl(iterator.hourInit),
-        hourFinish: new FormControl(iterator.hourFinish),
-        numWeek: new FormControl(iterator.numWeek),
-    });}
+          this.reportForm = this.reportObject[iterator.technicianIdentity];
+          this.reportForm = new FormGroup({
+            //   reportIdentityNumber: new FormControl(e.reportIdentityNumber),
+            technicianIdentity: new FormControl(
+              iterator.technicianIdentity.value || '',
+              Validators.required
+            ),
+            dateInit: new FormControl(iterator.dateInit),
+            dateFinish: new FormControl(iterator.dateFinish),
+            hourInit: new FormControl(iterator.hourInit),
+            hourFinish: new FormControl(iterator.hourFinish),
+            numWeek: new FormControl(iterator.numWeek),
+          });
+        }
+      });
+    }
+  }
 
-
-
-  
-  });
-}
- }
+  noWhitespaceValidator(control: FormControl) {
+    const isWhitespace = (control.value || '').trim().length === 0;
+    const isValid = !isWhitespace;
+    return isValid ? null : { whitespace: true };
+  }
 
   onSubmit(formDirective: FormGroupDirective): void {
     this.generateUUID();
-    if(formDirective.value.dateFinish<formDirective.value.dateInit){
-      alert("La fecha de finalización debe ser superior a la de inicion ")
-    }else{
+    if (
+      formDirective.value.dateFinish < formDirective.value.dateInit ||
+      formDirective.value.hourInit < formDirective.value.hourFinish
+    ) {
+      alert('La fecha de finalización debe ser superior a la de inicion ');
+    } else {
+      if (this.msgCalculator != null && this.msgCalculator > 0) {
+        let dateString = formDirective.value.dateInit;
+        let newDate = new Date(dateString);
+        var result = this.getWeekNumber(newDate);
+        console.log(result);
+        this.reportForm.value.weekNum = result;
 
-     if(this.msgCalculator!=null && this.msgCalculator>0){
-      let dateString = formDirective.value.dateInit;
-      let newDate = new Date(dateString);
-      var result = this.getWeekNumber(newDate);
-      console.log(result);
-      this.reportForm.value.weekNum = result;
+        this.reportService
+          .update(this.msgCalculator, this.reportForm.value)
+          .subscribe(
+            (response) => {
+              console.log(response);
+            },
+            (error) => {
+              console.log(error);
+            }
+          );
+      } else {
+        let dateString = formDirective.value.dateInit;
+        let newDate = new Date(dateString);
+        console.log(dateString);
+        console.log(newDate);
 
-  
-      this.reportService
-      .update(this.msgCalculator, this.reportForm.value)
-      .subscribe(
-        (response) => {
-          console.log(response);
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
-     } else{
+        var result = this.getWeekNumber(newDate);
+        console.log(result);
+        this.reportForm.value.numWeek = result;
 
-    let dateString = formDirective.value.dateInit;
-    let newDate = new Date(dateString);
-    console.log(dateString);
-    console.log(newDate);
+        console.log(this.reportForm.value.reportIdentityNumber);
 
-    var result = this.getWeekNumber(newDate);
-    console.log(result);
-    this.reportForm.value.numWeek = result;
- 
-      console.log(this.reportForm.value.reportIdentityNumber);
+        this.reportForm.value.reportIdentityNumber = this.uuidValue;
+        console.log(this.reportForm.value);
+        this.reportService
+          .create(this.reportForm.value)
+          .subscribe((response) => {
+            console.log(response);
+          });
 
-      this.reportForm.value.reportIdentityNumber = this.uuidValue;
-      console.log(this.reportForm.value);
-      this.reportService.create(this.reportForm.value).subscribe((response) => {
-        console.log(response);
-      });
-  
-this.reportForm.reset()  
-
-}}
-this.contador = 0;
-
-}
+        this.reportForm.reset();
+      }
+    }
+    this.contador = 0;
+  }
   getWeekNumber(d: any) {
     d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
     d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
@@ -119,8 +136,8 @@ this.contador = 0;
       console.log(data);
     });
   }
-  generateUUID(){
-    this.uuidValue=UUID.UUID();
+  generateUUID() {
+    this.uuidValue = UUID.UUID();
     return this.uuidValue;
   }
 }
